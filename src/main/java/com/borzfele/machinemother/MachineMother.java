7f67b465709ddbdcb1ad45050092e45d44a14a0a
borzfele <borzfele@gmail.com>
2018-01-17 19:38:35 +0100

    environment

---
+++ src/main/java/com/borzfele/machinemother/MachineMother.java
@@ -0,0 +1,12 @@
+package com.borzfele.machinemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineMother {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachineMother.class, args);
+    }
+}
