5daada115949ff4ae022141456c5e6d1c1beef2c
borzfele <borzfele@gmail.com>
2018-01-28 17:23:13 +0100

    TransactionController constructor added

--- src/main/java/com/borzfele/machinemother/logic/TransactionController.java
+++ src/main/java/com/borzfele/machinemother/logic/TransactionController.java
@@ -1,7 +1,20 @@
 package com.borzfele.machinemother.logic;
 
+import com.borzfele.machinemother.dao.TransactionService;
+import com.borzfele.machinemother.repository.TransactionRepository;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class TransactionController {
+
+    private TransactionService transactionService;
+    private TransactionRepository transactionRepository;
+
+    public TransactionController(TransactionService transactionService, TransactionRepository transactionRepository) {
+        this.transactionService = transactionService;
+        this.transactionRepository = transactionRepository;
+    }
+
+
+
 }
