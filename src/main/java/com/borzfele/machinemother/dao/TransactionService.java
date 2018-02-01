8dafd47c758167c0bf663d3abee4f52f45d84d53
borzfele <borzfele@gmail.com>
2018-02-01 11:04:12 +0100

    sum of all income and sum of income by day added

--- src/main/java/com/borzfele/machinemother/dao/TransactionService.java
+++ src/main/java/com/borzfele/machinemother/dao/TransactionService.java
@@ -5,6 +5,9 @@ import com.borzfele.machinemother.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -21,8 +24,40 @@ public class TransactionService {
             return TransactionRepository.findAll();
         }
 
-        public  Transaction findById(Long id) {
+        public Transaction findById(Long id) {
             return TransactionRepository.findOne(id);
+        }
+
+        public List<Transaction> findByDate(Date date) {
+            return TransactionRepository.findByDate(date);
+        }
+
+        public long getSumOfIncome() {
+            long sumOfIncome = 0;
+            List<Transaction> transactions = new ArrayList<Transaction>();
+            transactions = getAll();
+
+            for (Transaction transaction : transactions) {
+                if (transaction.getValue() > 0) {
+                    sumOfIncome += transaction.getValue();
+                }
+            }
+
+            return sumOfIncome;
+        }
+
+        public long getSumOfIncomeByDay(Date date) {
+            long sumOfIncome = 0;
+            List<Transaction> incomesByDay = new ArrayList<Transaction>();
+            incomesByDay = findByDate(date);
+
+            for (Transaction transaction : incomesByDay) {
+                if (transaction.getValue() > 0) {
+                    sumOfIncome += transaction.getValue();
+                }
+            }
+
+            return sumOfIncome;
         }
 
 }
--- src/main/java/com/borzfele/machinemother/logic/HomeController.java
+++ src/main/java/com/borzfele/machinemother/logic/HomeController.java
@@ -1,5 +1,7 @@
 package com.borzfele.machinemother.logic;
 
+import com.borzfele.machinemother.dao.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private TransactionService transactionService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String renderIndex(Model model) {
